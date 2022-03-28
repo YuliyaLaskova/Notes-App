18f0ed95203b5baf554d574df888d5972ae7c819
Yuliya <101642263+YuliyaLaskova@users.noreply.github.com>
2022-03-28 16:32:23 +0300

    App is ready

--- Notes App/ViewController.swift
+++ Notes App/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var readyButton: UIButton!
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var mainTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray6
+        
+        titleTextField.layer.cornerRadius = 10
+        titleTextField.backgroundColor = .systemGray4
+        titleTextField.font = .boldSystemFont(ofSize: 22)
+        titleTextField.textColor = .black
+        titleTextField.adjustsFontSizeToFitWidth = true
+        titleTextField.placeholder = "Write the title"
+        
+        mainTextField.layer.cornerRadius = 10
+        mainTextField.backgroundColor = .systemGray4
+        mainTextField.font = .systemFont(ofSize: 14)
+        mainTextField.textColor = .black
+        
+        mainTextField.becomeFirstResponder()
+        
+        readyButton.layer.cornerRadius = 5
+        readyButton.tintColor = .black
+        readyButton.backgroundColor = .systemBlue
+        
     }
 
-
+    @IBAction func readyButtonPressed() {
+        titleTextField.resignFirstResponder()
+        mainTextField.resignFirstResponder()
+    }
+    
 }
 
