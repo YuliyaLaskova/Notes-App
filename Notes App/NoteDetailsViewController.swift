8ea94ec162657459f7647738e00024c0ca64fc04
Yuliya <101642263+YuliyaLaskova@users.noreply.github.com>
2022-03-31 13:26:14 +0300

    lint corrected version

--- Notes App/NoteDetailsViewController.swift
+++ Notes App/NoteDetailsViewController.swift
@@ -10,43 +10,40 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var readyButton: UIButton!
-    
+
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var mainTextField: UITextView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupUI()
-        
+
     }
 
     @IBAction func readyButtonPressed() {
         view.endEditing(true)
     }
-    
+
   private func setupUI() {
         view.backgroundColor = .systemGray6
-        
+
         titleTextField.layer.cornerRadius = 10
         titleTextField.backgroundColor = .systemGray5
         titleTextField.font = .boldSystemFont(ofSize: 22)
         titleTextField.textColor = .black
         titleTextField.adjustsFontSizeToFitWidth = true
         titleTextField.placeholder = "Write the title"
-        
+
         mainTextField.layer.cornerRadius = 10
         mainTextField.backgroundColor = .systemGray5
         mainTextField.font = .systemFont(ofSize: 14)
         mainTextField.textColor = .black
-        
+
         mainTextField.becomeFirstResponder()
-        
+
         readyButton.layer.cornerRadius = 5
         readyButton.tintColor = .black
         readyButton.backgroundColor = .systemBlue
     }
 }
-
-
-
