f5b3f4916e7637a1cbb51f057d10ff92f0b26e97
Yuliya <101642263+YuliyaLaskova@users.noreply.github.com>
2022-06-02 18:38:58 +0300

    реализация удаления ячеек без свайпа

--- Notes App/ListViewController.swift
+++ Notes App/ListViewController.swift
@@ -200,17 +200,20 @@ class ListViewController: UIViewController {
 
     @objc func deleteButtonPressed() {
         if let selectedRows = self.tableView.indexPathsForSelectedRows, !(selectedRows.isEmpty) {
-            for var selectionIndex in selectedRows {
-                while selectionIndex.item >= notes.count {
-                    selectionIndex.item -= 1
-                }
-                tableView(tableView, commit: .delete, forRowAt: selectionIndex)
-            }
+            for index in selectedRows {
+                            _ = updateModel(index: index, isChecked: true)
+                        }
+                        notes = notes.filter({ !$0.isChecked })
+                        tableView.reloadData()
             toggleTableEditingMode()
         } else {
             showAlert(message: "Вы не выбрали ни одной заметки", title: "Ошибка")
         }
     }
+
+    private func updateModel(index: IndexPath, isChecked: Bool) -> NoteDataModel {
+            notes[index.row].update(index: index, isChecked: isChecked)
+        }
 }
 
 // MARK: TableView extentions
@@ -224,29 +227,18 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: noteCell, for: indexPath)
                 as? NoteCell else { return UITableViewCell() }
 
-        let note = notes[indexPath.row]
+        let note = updateModel(index: indexPath, isChecked: cell.isSelected)
         cell.setup(with: note)
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let note = notes[indexPath.row].update(index: indexPath)
-        if tableView.isEditing == false {
-            showNoteDetailsViewController(for: note)
-        }
-    }
-
-    func tableView(
-        _ tableView: UITableView,
-        commit editingStyle: UITableViewCell.EditingStyle,
-        forRowAt indexPath: IndexPath
-    ) {
-        if editingStyle == .delete {
-            tableView.beginUpdates()
-            notes.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .left)
-            tableView.endUpdates()
-        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: noteCell, for: indexPath)
+                        as? NoteCell else { return }
+                let note = updateModel(index: indexPath, isChecked: cell.isSelected)
+                if !tableView.isEditing {
+                    showNoteDetailsViewController(for: note)
+                }
     }
 }
 
